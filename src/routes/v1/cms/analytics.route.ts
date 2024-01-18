5e69051d6adb2f6089a13147ebda311e41941025
yucheng2207 <e0560244@u.nus.edu>
2024-01-18 16:21:24 +0800

    add validation for analytics route

--- src/routes/v1/cms/analytics.route.ts
+++ src/routes/v1/cms/analytics.route.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import cmsAnalyticsController from "../../../controllers/cms/analytics.controller";
+import validate from "../../../middlewares/validate";
+import analyticsValidation from "../../../validations/cms/analytics.validation";
 
 const router = express.Router();
 
-router.get("/campaigns", cmsAnalyticsController.listCampaigns);
+router.get("/campaigns", 
+    validate(analyticsValidation.listCampaigns),
+    cmsAnalyticsController.listCampaigns);
 
-router.get("/most-popular-amounts", cmsAnalyticsController.getMostPopularAmounts);
+router.get("/most-popular-amounts", 
+    validate(analyticsValidation.getMostPopularAmounts),
+    cmsAnalyticsController.getMostPopularAmounts);
 
-router.get("/detail-campaigns", cmsAnalyticsController.getAllCampaignInformation);
+router.get("/detail-campaigns", 
+    validate(analyticsValidation.getAllCampaignInformation),
+    cmsAnalyticsController.getAllCampaignInformation);
 
 router.get("/:campaignId", cmsAnalyticsController.getCampaignInformation);
 
